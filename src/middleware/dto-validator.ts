3533c33af6d9bcf5a675caee34e7d73ce4df9b01
Nakul Jhunjhunwala <jhunjhunwalanakul@gmail.com>
2024-10-18 23:48:50 +0530

    Update dto-validator.ts

--- src/middleware/dto-validator.ts
+++ src/middleware/dto-validator.ts
@@ -6,10 +6,10 @@ import { Request, Response, NextFunction } from 'express';
 
 export function validateRequest<T>(dtoClass: any) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const dtoObj = plainToInstance(dtoClass, req.body, {
-      excludeExtraneousValues: true,
+    const dtoObj = plainToInstance(dtoClass, req.body);
+    const errors: ValidationError[] = await validate(dtoObj,{
+      whitelist: true
     });
-    const errors: ValidationError[] = await validate(dtoObj);
 
     if (errors.length > 0) {
       const messages = errors
