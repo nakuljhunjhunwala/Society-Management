c2556daab53fc8772d4dcbb9531ee96f29815f58
Nakul Jhunjhunwala <jhunjhunwalanakul@gmail.com>
2024-10-19 00:03:05 +0530

    Update user routes

--- src/module/user/user.route.ts
+++ src/module/user/user.route.ts
@@ -9,7 +9,48 @@ const wrappedUserController = new WrapperClass(
   new UserController(),
 ) as unknown as UserController & { [key: string]: any };
 
+/**
+ * @swagger
+ * /user/me:
+ *   get:
+ *     summary: Get current user information
+ *     tags: [User]
+ *     parameters:
+ *      - $ref: '#/components/parameters/DeviceTokenHeader'
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved user information
+ *       401:
+ *         description: Unauthorized
+ */
 router.get('/me', authMiddleware, wrappedUserController.me);
+
+/**
+ * @swagger
+ * /user/me:
+ *   put:
+ *     summary: Update current user information
+ *     tags: [User]
+ *     parameters:
+ *      - $ref: '#/components/parameters/DeviceTokenHeader'
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdateMe'
+ *     responses:
+ *       200:
+ *         description: Successfully updated user information
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
 router.put(
   '/me',
   authMiddleware,
